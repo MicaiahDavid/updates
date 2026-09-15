8f23561999b864f552ab875479934d7cc7ca81ca
MicaiahDavid <spencer.sundem@proton.me>
2026-09-14 20:58:02 -0700

    Add files via upload

---
+++ images/data.js
@@ -0,0 +1,152 @@
+const siteData = {
+  "site": {
+    "title": "Micaiah David",
+    "subtitle": "Updates on prognosis, treatments, and prayer requests",
+    "shareMessage": "Please feel free to share this page with anyone who would be willing to pray for our boy.",
+    "heroImage": "images/unnamed.jpg",
+    "heroImagePosition": "[23%_43%]"
+  },
+  "sidebar": {
+    "treatmentMilestones": [
+      {
+        "label": "Diagnosis",
+        "value": "Retinoblastoma"
+      },
+      {
+        "label": "Chemotherapy Round",
+        "value": "Round 4 (9/8-9 infusions)"
+      },
+      {
+        "label": "Cancer Location",
+        "value": "Contained to the eyes"
+      },
+      {
+        "label": "Cause",
+        "value": "Genetic mutation during embryonic development"
+      },
+      {
+        "label": "Next Exam",
+        "value": "9/23"
+      }
+    ],
+    "howToPray": [
+      "That the right eye would continue to improve beyond all expectations",
+      "That the left eye would dramatically improve and the retina would reattach to the back wall",
+      "Strength and comfort for Micaiah",
+      "That Micaiah would stay healthy and avoid nausea",
+      "Strength and Understanding for Elias",
+      "Strength for our family"
+    ],
+    "howToGiveThanks": [
+      "Micaiah's left eye changed color, alerting us to the issue",
+      "Micaiah's right eye has responded well to treatment so far",
+      "Long term effects of treatment should be minimal",
+      "Micaiah's genetics resulted from a mutation in embryonic development, so it does not affect the other boys",
+      "So far Micaiah has handled chemo like a champ",
+      "Tests have shown no cancer beyond the globes of the eyes",
+      "Easy birth and transition for Samuel",
+    ]
+  },
+  "blogPosts": [
+    {
+      "id": 11,
+      "title": "Reflections from Jerusha",
+      "date": "September 14, 2026",
+      "excerpt": "We find ourselves in an off-week and with not much that has changed in Micaiah's status and demeanor, so I thought I might jump in to give some insights from my side of things. We just started round four of chemo last week, and the next big step is our big eye exam under anesthesia on September 23rd.",
+      "heroImage": "images/1000002522.jpg",
+      "heroImagePosition": "center",
+      "color": "purple"
+    },
+    {
+      "id": 10,
+      "title": "The Naive and the Miraculous",
+      "date": "August 16, 2026",
+      "excerpt": "Treatment continues apace as Micaiah's body is handling the infusions well. He has very little nausea for the few days after chemo, and his blood counts have been recovering well so far. Elias' simple prayer on a backpacking trip reminded us to pray audaciously to the One who loves to shower us with goodness.",
+      "heroImage": "images/20260810_091005.jpg",
+      "heroImagePosition": "center",
+      "color": "green"
+    },
+    {
+      "id": 9,
+      "title": "Long Road Ahead",
+      "date": "July 23, 2026",
+      "excerpt": "We have officially received our first status update for Micaiah's retinoblastoma. The good news is that Micaiah's right eye is responding excellently to the treatment—the tumor mass has shrunk by about 50%. The left eye is still tenuous, but we believe in a God who specializes in miracles.",
+      "heroImage": "images/20260722_092505.jpg",
+      "heroImagePosition": "center",
+      "color": "blue"
+    },
+    {
+      "id": 8,
+      "title": "Round 2 Begins",
+      "date": "July 15, 2026",
+      "excerpt": "We committed early on to enjoy God's blessings through this trial and avoid catastrophizing our present moment with possible future challenges. Still, as we get closer to Micaiah's next eye exam, we gradually become more apprehensive. Round 2 of treatment began 7/14, and Micaiah handled everything very well—even the port access.",
+      "heroImage": "images/mitene16673be4-e824-4203-98f8-b1b8638c2a68.jpg",
+      "heroImagePosition": "center",
+      "color": "purple"
+    },
+    {
+      "id": 7,
+      "title": "New Baby and Labs",
+      "date": "June 30, 2026",
+      "excerpt": "Weighing 8 pounds 3 ounces and measuring 20.25 inches, Samuel Halvor was born on June 27, 2026, at 12:19 p.m. He and Jerusha are home now and recovering well. Micaiah's blood counts have bottomed out and are starting to recover, and we've noticed a reduction in the darker eye color of his left eye—a possible sign of treatment efficacy.",
+      "heroImage": "images/mitenec94d46f2-72e4-4c15-bf34-a8ae7c6155d1.jpg",
+      "heroImagePosition": "center",
+      "color": "green"
+    },
+    {
+      "id": 6,
+      "title": "God is Over All",
+      "date": "June 24, 2026",
+      "excerpt": "Four days overdue, and still no baby. We've had a lot of uncertainty and a large dose of hurry-up-and-wait since our last update. Meanwhile, Micaiah is handling his treatment very well. His appetite has been good, and he hasn't been significantly nauseous. We received the report from the marrow biopsy—everything was negative! Right now, the doctor tells us that the globes of the eyes are the only area of concern.",
+      "heroImage": "images/20260624_091351.jpg",
+      "heroImagePosition": "center",
+      "color": "blue"
+    },
+    {
+      "id": 5,
+      "title": "Round 1 Infusions and Sundries",
+      "date": "June 18, 2026",
+      "excerpt": "Our hearts are tired, but full. The news we have received so far this week has been a mix of good and unclear news. So far we haven't received any bad news, but we have a long road ahead of us. This is just the beginning of our journey, so we are preparing ourselves mentally and spiritually for the days to come.",
+      "heroImage": "images/mitene24463927-bccd-4e1b-8b73-cd6117bc9d7b.jpg",
+      "heroImagePosition": "center",
+      "color": "orange"
+    },
+    {
+      "id": 4,
+      "title": "Schedule Updates",
+      "date": "June 16, 2026",
+      "excerpt": "The week leading up to Baby #3 (due Saturday, 6/20) is filling up quickly. It's quite remarkable, actually, how the timing of everything has worked out. Although we regret we didn't find out about Micaiah's tumors much earlier, it's hard not to see God making a way for us.",
+      "heroImage": "images/back porch.jpg",
+      "heroImagePosition": "center",
+      "color": "blue"
+    },
+    {
+      "id": 3,
+      "title": "MRI Results and Treatment Schedule",
+      "date": "June 12, 2026",
+      "excerpt": "Today was another step in the process. While Elias and I went into work, Jerusha took Micaiah for his MRI appointment as well as a hematology and oncology consultation. It was a long day for Micaiah. He left home before 8am and didn't get home until 6pm. Since they put him under for the MRI, he wasn't able to eat anything throughout the morning.",
+      "heroImage": "images/1000013115.jpg",
+      "heroImagePosition": "[50%_30%]",
+      "color": "green"
+
+    },
+    {
+      "id": 2,
+      "title": "How We're Doing",
+      "date": "June 11, 2026",
+      "excerpt": "What we're experiencing now is a calm before the storm. Of course we are in emotional turmoil, but as far as daily life goes, there's not a whole lot we can do for Micaiah until treatment is implemented. We've done a lot of thinking and processing. Last night, Jerusha and I got to finally sit down and connect over all this.",
+      "heroImage": "images/unnamed (1).jpg",
+      "heroImagePosition": "[80%_30%]",
+      "color": "purple"
+    },
+    {
+      "id": 1,
+      "title": "Micaiah's Diagnosis",
+      "date": "June 11, 2026",
+      "excerpt": "A few months ago, we started to notice that Micaiah's eyes were slightly different shades of blue—one darker than the other. I thought it was a fun physical trait that would be a go-to ice breaker for him while making friends. Jerusha's motherly instincts, however, turned out to be correct. The optometrist ran some tests and, according to Jerusha, became visibly disturbed, giving us an \"urgent referral\" to Seattle Children's.",
+      "heroImage": "images/1000013003.jpg",
+      "heroImagePosition": "[53%_33%]",
+      "color": "blue",
+      "isStart": true    }
+  ]
+};
